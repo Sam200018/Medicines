0da153d0f36c4862c85c2135c6e70dfeda434d74
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-27 11:04:29 -0600

    pills logo implemented

---
+++ lib/ui/pages/login/widgets/pills_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LogoPills extends StatelessWidget {
+  const LogoPills({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/logo_pills.png");
+  }
+}
