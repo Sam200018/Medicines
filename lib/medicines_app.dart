50c8f74cd81f81eddb67d1b8a83ad428c7ca0737
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-28 17:15:59 -0600

    inject a dependency

--- lib/medicines_app.dart
+++ lib/medicines_app.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medicines/config/router/router.dart';
 import 'package:medicines/config/theme/theme.dart';
+import 'package:medicines/ui/pages/signup/Bloc/signup_bloc.dart';
 
 class MedicinesApp extends StatelessWidget {
   const MedicinesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "Medicines",
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      routerConfig: MedicinesRouter().router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SignupBloc>(create: (context) => SignupBloc()),
+      ],
+      child: MaterialApp.router(
+        title: "Medicines",
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        routerConfig: MedicinesRouter().router,
+      ),
     );
   }
 }
