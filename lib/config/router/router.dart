5fd4e4a0993c7dd142c1890ccf0560dd8430dbba
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-28 17:16:52 -0600

    new route added

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:medicines/ui/pages/login/login_page.dart';
+import 'package:medicines/ui/pages/signup/signup_page.dart';
 
 class MedicinesRouter {
   static String home = "/";
   static String login = "/login";
+  static String signup = "/signup";
 
   GoRouter router = GoRouter(
     initialLocation: login,
@@ -11,6 +13,10 @@ class MedicinesRouter {
       GoRoute(
         path: login,
         builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: signup,
+        builder: (context, state) => const SignupPage(),
       ),
     ],
   );
