4b1ed271f28235866cdaa9fc89f26d237d7983e8
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-28 17:14:58 -0600

    mini logo added

---
+++ lib/ui/pages/signup/widgets/mini_logo_pills.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MiniLogoPills extends StatelessWidget {
+  const MiniLogoPills({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/mini_logo_pills.png");
+  }
+}
