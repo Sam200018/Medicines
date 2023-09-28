ba4975898c7c8d144acd63ea6337c376b1c475a4
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-28 17:16:55 -0600

    new route added

---
+++ lib/ui/pages/signup/signup_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:medicines/config/constants/colors.dart';
+import 'package:medicines/config/router/router.dart';
+import 'package:medicines/ui/pages/login/widgets/pills_button.dart';
+import 'package:medicines/ui/pages/login/widgets/pills_input.dart';
+import 'package:medicines/ui/pages/login/widgets/pills_text_button.dart';
+import 'package:medicines/ui/pages/signup/widgets/mini_logo_pills.dart';
+
+import 'Bloc/signup_bloc.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignupBloc, SignupState>(
+      builder: (context, state) {
+        return Scaffold(
+          bottomNavigationBar: BottomAppBar(
+            child: PillsTextButton(
+              text: "Ya tengo cuenta",
+              fontSize: 20.0,
+              onPress: () {
+                context.go(MedicinesRouter.login);
+              },
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+              child: Column(
+                children: [
+                  const MiniLogoPills(),
+                  const SizedBox(height: 20),
+                  PillsInput(
+                    textController: context.read<SignupBloc>().firstNameCtrl,
+                    textInputType: TextInputType.name,
+                    labelText: "Nombre",
+                    errorText: state.nameError,
+                    onChanged: (name) {
+                      context.read<SignupBloc>().add(const NameChanged());
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  PillsInput(
+                    textController: context.read<SignupBloc>().lastNameCtrl,
+                    textInputType: TextInputType.name,
+                    labelText: "Apellido",
+                    errorText: state.lastNameError,
+                    onChanged: (lastName) {
+                      context.read<SignupBloc>().add(const LastNameChanged());
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  PillsInput(
+                    textController: context.read<SignupBloc>().emailCtrl,
+                    textInputType: TextInputType.emailAddress,
+                    labelText: "Email",
+                    errorText: state.emailError,
+                    onChanged: (email) {
+                      context.read<SignupBloc>().add(const EmailChanged());
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  PillsInput(
+                    textController: context.read<SignupBloc>().passwordCtrl,
+                    textInputType: TextInputType.emailAddress,
+                    labelText: "Password",
+                    obscureText: true,
+                    errorText: state.passwordError,
+                    onChanged: (password) {
+                      context.read<SignupBloc>().add(const PasswordChanged());
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  PillsInput(
+                    textController:
+                        context.read<SignupBloc>().verifyPasswordCtrl,
+                    textInputType: TextInputType.emailAddress,
+                    obscureText: true,
+                    labelText: "Confirmar password",
+                    errorText: state.verPasswordError,
+                    onChanged: (verPassword) {
+                      context
+                          .read<SignupBloc>()
+                          .add(const VerifiedPasswordChanged());
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  PillsButton(
+                    onPress:
+                        isSignupButtonEnable(state, context) ? () {
+
+                        } : null,
+                    text: "Crear cuenta",
+                    height: 60,
+                    color: mainButton,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  bool isSignupButtonEnable(SignupState state, BuildContext context) {
+    final bloc = context.read<SignupBloc>();
+
+    return state.isFormValid &&
+        !state.isSubmitting &&
+        bloc.firstNameCtrl.text.isNotEmpty &&
+        bloc.lastNameCtrl.text.isNotEmpty &&
+        bloc.passwordCtrl.text.isNotEmpty &&
+        bloc.verifyPasswordCtrl.text.isNotEmpty;
+  }
+}
