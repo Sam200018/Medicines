ee4feaa9350b89b7f34f451fe7fa5ba6180806aa
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-02 16:46:20 -0600

    auth repository updated

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -2,4 +2,14 @@ import 'package:dio/src/form_data.dart';
 
 abstract class AuthRepository {
   Future<String> signup(FormData formData);
+
+  Future<String> login(FormData formData);
+
+  Future<void> saveToken(String token);
+
+  Future<String?> getToken();
+
+  Future<void> saveEmail(String email);
+
+  Future<String?> getEmail();
 }
--- lib/infrastructure/repositories/auth_repository_impl.dart
+++ lib/infrastructure/repositories/auth_repository_impl.dart
@@ -1,12 +1,34 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:medicines/domain/datasources/auth_datasource.dart';
 import 'package:medicines/domain/repositories/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthDataSource authDataSource;
+  final FlutterSecureStorage flutterSecureStorage;
 
-  AuthRepositoryImpl(this.authDataSource);
+  AuthRepositoryImpl({
+    required this.authDataSource,
+    required this.flutterSecureStorage,
+  });
 
   @override
   Future<String> signup(FormData formData) => authDataSource.signup(formData);
+
+  @override
+  Future<String> login(FormData formData) => authDataSource.login(formData);
+
+  @override
+  Future<String?> getToken() => flutterSecureStorage.read(key: "Token");
+
+  @override
+  Future<void> saveToken(String token) =>
+      flutterSecureStorage.write(key: "Token", value: token);
+
+  @override
+  Future<String?> getEmail() => flutterSecureStorage.read(key: "Email");
+
+  @override
+  Future<void> saveEmail(String email) =>
+      flutterSecureStorage.write(key: "Email", value: email);
 }
