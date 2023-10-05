b7d16c23c9114df9b59ded1d0ff2a4b28405c3bc
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-05 16:52:35 -0600

    loading_page.dart added

---
+++ lib/ui/pages/Loading/loading_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
