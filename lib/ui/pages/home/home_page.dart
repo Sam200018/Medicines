8c7b1f027ecd65b8eb06cb2f6cf7a400c95584f0
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-11 18:50:05 -0600

    home page separated in two and updated

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:medicines/config/constants/colors.dart';
-import 'package:medicines/ui/pages/login/widgets/pills_button.dart';
-
-import 'auth/auth_bloc.dart';
+import 'package:medicines/ui/pages/home/at_home/at_home_page.dart';
+import 'package:medicines/ui/pages/home/not_at_home/not_at_home_page.dart';
+import 'home_bloc/home_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: PillsButton(
-        onPress: () {
-          context.read<AuthBloc>().add(const LogoutRequested());
-        },
-        text: 'Salir',
-        height: 50.0,
-        color: mainButton,
-      )),
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state is AtHome) {
+          return const AtHomePage();
+        }
+        if (state is NotAtHome) {
+          return const NotAtHomePage();
+        }
+
+        return const Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        );
+      },
     );
   }
 }
