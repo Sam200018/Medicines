488439a1bd767e8f32d32b62854c5eddfde3d8c0
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-16 11:00:55 -0600

    home page with snackbar

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medicines/ui/pages/home/at_home/at_home_page.dart';
 import 'package:medicines/ui/pages/home/not_at_home/not_at_home_page.dart';
+import '../login/widgets/pills_snackbar.dart';
 import 'home_bloc/home_bloc.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,19 +10,40 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeBloc, HomeState>(
-      builder: (context, state) {
-        if (state is AtHome) {
-          return const AtHomePage();
+    return BlocListener<HomeBloc, HomeState>(
+      listener: (context, state) {
+        if (state.message != null) {
+          if (state.isFailure!) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(PillsSnackbar(message: state.message!));
+          }
+          if (state.isSuccess!) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                PillsSnackbar(
+                  message: state.message!,
+                  backgroundColor: Colors.green,
+                ),
+              );
+          }
         }
-        if (state is NotAtHome) {
-          return const NotAtHomePage();
-        }
-
-        return const Scaffold(
-          body: Center(child: CircularProgressIndicator()),
-        );
       },
+      child: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          if (state is AtHome) {
+            return const AtHomePage();
+          }
+          if (state is NotAtHome) {
+            return const NotAtHomePage();
+          }
+
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        },
+      ),
     );
   }
 }
