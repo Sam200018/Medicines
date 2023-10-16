16880dc49d7797c8e6dca6a54ce0fb7612a9ae61
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-16 10:54:42 -0600

    not at home page implemented

--- lib/ui/pages/home/not_at_home/not_at_home_page.dart
+++ lib/ui/pages/home/not_at_home/not_at_home_page.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medicines/config/constants/colors.dart';
+import 'package:medicines/ui/pages/home/widgets/pills_button_icon.dart';
+
+import '../home_bloc/home_bloc.dart';
 
 class NotAtHomePage extends StatelessWidget {
   const NotAtHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Not at Home"),
+    return Scaffold(
+      endDrawer: Drawer(
+        child: ListView(),
+      ),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(90),
+        child: AppBar(
+          toolbarHeight: 70,
+          title: Image.asset(
+            "assets/logo_menu.png",
+            scale: 2,
+          ),
+
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          children: [
+            PillsButtonIcon(
+              onPress: () {
+                context.read<HomeBloc>().add(CreateHome());
+              },
+              text: "Crear una casa",
+              height: 100,
+              color: createHouseButton,
+              icon: Icons.home,
+              iconSize: 70,
+            ),
+            const SizedBox(height: 20),
+            PillsButtonIcon(
+                onPress: () {},
+                text: "Unirse a una casa",
+                height: 100,
+                color: joinHouseButton,
+                icon: Icons.add_home_work_outlined,
+                iconSize: 70)
+          ],
+        ),
       ),
     );
   }
