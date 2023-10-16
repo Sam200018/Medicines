59350e44d149103fb2c420d111cb387896d625b5
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-16 10:58:51 -0600

    new color in constants added

--- lib/config/constants/colors.dart
+++ lib/config/constants/colors.dart
@@ -2,3 +2,5 @@ import 'dart:ui';
 
 const Color mainButton = Color(0xffF78812);
 const Color mainButtonDisable = Color(0xffAC3D00);
+const Color createHouseButton = Color(0xff1EEF65);
+const Color joinHouseButton = Color(0xff1798E1);
