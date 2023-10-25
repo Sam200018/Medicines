f2fe85a949336e19f79585fea3d0f0b36aa271ea
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-25 17:16:05 -0600

    QR scanner page implemented

---
+++ lib/ui/pages/read_code/read_code_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ReadCodePage extends StatelessWidget {
+  const ReadCodePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lectura de código QR"),
+      ),
+      body: MobileScanner(
+        controller: MobileScannerController(
+            detectionSpeed: DetectionSpeed.normal,
+            facing: CameraFacing.back,
+            torchEnabled: true),
+        onDetect: (barcodes) {
+          print(barcodes);
+        },
+      ),
+    );
+  }
+}
