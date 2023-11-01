9b48088ae893a8f8343e79a96db9621e8325fb8a
Samuel Bautista <samuelbaubas@gmail.com>
2023-11-01 16:34:46 -0600

    medicines list added to home page and Medicine Item implemented

--- lib/ui/pages/home/at_home/at_home_page.dart
+++ lib/ui/pages/home/at_home/at_home_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:medicines/config/router/router.dart';
+import 'package:medicines/domain/entities/medicine.dart';
 import 'package:medicines/ui/pages/home/at_home/widgets/medicine_list.dart';
 import 'package:medicines/ui/pages/home/widgets/pills_drawer.dart';
 import 'package:medicines/ui/pages/home/widgets/pills_floating_action_button.dart';
 import 'package:medicines/ui/pages/login/widgets/pills_input.dart';
 
 class AtHomePage extends StatelessWidget {
-  const AtHomePage({super.key});
+  final List<Medicine> medicines;
+
+  const AtHomePage({super.key, required this.medicines});
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +26,12 @@ class AtHomePage extends StatelessWidget {
         ),
       ),
       endDrawer: const PillsDrawer(),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30.0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
         child: Center(
-          child: MedicineList()
-        ),
+            child: MedicineList(
+          medicines: medicines,
+        )),
       ),
       floatingActionButton: SizedBox(
         width: 180.0,
@@ -44,7 +48,7 @@ class AtHomePage extends StatelessWidget {
             PillsFloatingActionButton(
               tooltip: "FAB 2",
               onPress: () {
-                context.push(MedicinesRouter.medicine);
+                context.push("${MedicinesRouter.medicine}/");
               },
               label: const Icon(Icons.medical_services_outlined),
             ),
